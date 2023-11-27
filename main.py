63fdbe87cd80666332035f07c749bfde4a18112d
DhanushAdithiya <k.b.dhanush02@gmail.com>
2023-11-27 18:53:51 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import sys
+import numpy as np
+from PIL import Image
+from sklearn.cluster import KMeans
+
+
+image = Image.open("./colr.jpg")
+img_arr = np.array(image)
+dis_img = Image.fromarray(img_arr, "RGB")
+
+
+vec = img_arr.reshape((-1,3))
+print(img_arr.shape)
+print(vec.shape)
+
+
+kmeans = KMeans(n_clusters = 5, random_state = 0, n_init = 1).fit(vec)
+colors = np.uint8(kmeans.cluster_centers_)
+seg = colors[kmeans.labels_.flatten()]
+
+
+image_quant = seg.reshape(img_arr.shape)
+quant = Image.fromarray(image_quant, "RGB")
+
+
+quant.show()
+
+
